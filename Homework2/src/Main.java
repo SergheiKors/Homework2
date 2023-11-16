70736d865a80f61e2beadaf14c8d6801432dd148
Serghei <korsakov2975@gmail.com>
2023-11-16 18:29:54 +0300

    first task

---
+++ Homework2/src/Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int x = 800; //стоимость билета
+        int r = 20; //количество рублей за каждую бонусную милю
+        int d = x / r; // количество бонусных миль
+        System.out.println(d);
+    }
+}
